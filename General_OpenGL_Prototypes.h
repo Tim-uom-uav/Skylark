a6f763c4875efb37d79f1c9aed030098e010bf69
rafty4 <peter.naylor17@btinternet.com>
2019-02-11 11:15:21 +0000

    Delete General_OpenGL_Prototypes.h

--- General_OpenGL_Prototypes.h
+++
@@ -1,7 +0,0 @@
-#ifndef GENERAL_OPENGL_PROTOTYPES_H_
-#define GENERAL_OPENGL_PROTOTYPES_H_
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
-
-#endif // !GENERAL_OPENGL_PROTOTYPES_H_
